b748f0beb58380866696246c8167838f549dafd6
vzateychuk <vladimir.zateychuk@yandex.ru>
2020-07-07 04:01:55 +0300

    changed the center.properties name

--- run-asud.sh
+++ run-asud.sh
@@ -1,1 +1,7 @@
-$JDK8/bin/java -Dwebdriver.chrome.driver=$SOFT/chromedriver/chromedriver -Denv=local -jar target/webdriver-asud.jar
+if [ "$1" == "" ]; then
+    echo "ERROR! Parameter is empty. Expected 'local/as-center/center2'"
+    exit
+fi
+
+$JDK8/bin/java -Dwebdriver.chrome.driver=$SOFT/chromedriver/chromedriver.chrome.v83 -Denv=$1 -jar target/webdriver-asud.jar
+
