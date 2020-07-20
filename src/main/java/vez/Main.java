d1385e5e8e97abcc43e17f3f2e7fcec541594392
vzateychuk <vladimir.zateychuk@yandex.ru>
2020-07-20 23:55:01 +0300

    Обновлена версия webdriver-java

--- src/main/java/vez/Main.java
+++ src/main/java/vez/Main.java
@@ -15,7 +15,7 @@ import java.time.LocalTime;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         AbstractApplicationContext context = new AnnotationConfigApplicationContext(Beans.class);
         WebDriver driver = context.getBean("webDriverBean", WebDriver.class);
